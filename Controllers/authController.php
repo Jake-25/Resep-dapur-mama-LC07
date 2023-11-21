a632530f98a811e2f70c1ef6f45e4f0a2011ae0a
Jake-25 <96160692+Jake-25@users.noreply.github.com>
2023-11-22 03:09:49 +0700

    updated

--- Controllers/authController.php
+++ Controllers/authController.php
@@ -1,48 +1,70 @@
 <?php
 // authController.php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 session_start();
 
- // Sertakan konfigurasi database
+// Sertakan konfigurasi database & validasi
 require('connection.php');
 require('ValidateFUnction.php');
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Validasi dan sanitasi input
     $username = htmlspecialchars($_POST["username"]);
     $password = htmlspecialchars($_POST["password"]);
 
     if (!validateUsername($username)) {
-        echo "Invalid username format. Please use only letters, numbers, and underscores.";
+        echo "Format nama pengguna tidak valid. Gunakan hanya huruf, angka, dan garis bawah.";
         exit();
     }
 
     // Validasi password menggunakan fungsi yang baru ditambahkan
     if (!validatePassword($password)) {
-        echo "Invalid password format. Password must be at least 8 characters long.";
+        echo "Format kata sandi tidak valid. Kata sandi harus setidaknya 8 karakter.";
         exit();
     }
 
-
     // Persiapkan pernyataan SQL untuk mencari pengguna dengan username yang diberikan
-    $stmt = $conn->prepare("SELECT * FROM users WHERE username=? AND password=?;");
+    $stmt = $conn->prepare("SELECT id, username, password, session_id FROM users WHERE username=? AND password=?;");
     $stmt->bind_param("ss", $username, $password);
     $stmt->execute();
-    $stmt->bind_result($dbUsername, $dbPassword);
+    $stmt->bind_result($userId, $dbUsername, $dbPassword, $storedSessionId);
     $stmt->fetch();
     $stmt->close();
 
+
+
     // Verifikasi password
     if ($dbUsername && password_verify($password, $dbPassword)) {
+        // Check if the stored session ID matches the current session ID
+        if (!empty($storedSessionId) && $storedSessionId !== session_id()) {
+            // Session ID berbeda, logout otomatis pengguna
+            session_destroy();
+            echo "Anda sudah login dari perangkat atau sesi yang berbeda. Silakan login kembali.";
+            exit();
+        }
+
         // Login berhasil, atur sesi atau tindakan lainnya
         $_SESSION["username"] = $dbUsername;
+
+        // Update stored session ID in the database
+        $newSessionId = session_id();
+        $stmt = $conn->prepare("UPDATE users SET session_id=? WHERE id=?;");
+        $stmt->bind_param("si", $newSessionId, $userId);
+        $stmt->execute();
+        $stmt->close();
+
         header("Location: /profile.php"); // Redirect ke halaman profil
         exit();
     } else {
         // Login gagal, tampilkan pesan kesalahan
-        echo "Login failed. Check your username and password.";
+        echo "Login gagal. Periksa nama pengguna dan kata sandi Anda.";
     }
 
-    // Tutup koneksi ke database
-    $conn->close();
+        // Tutup koneksi ke database
+        $conn->close();
+        
 }
 ?>
---
+++ Controllers/cryption.php
@@ -0,0 +1,43 @@
+<?php
+$envFile = __DIR__ . '/.env';
+
+if (!file_exists($envFile)) {
+    die('File .env tidak ditemukan. Pastikan file .env.example sudah disalin dan diatur.');
+}
+
+// Baca nilai variabel dari file .env
+$envConfig = parse_ini_file($envFile);
+
+// Ambil kunci enkripsi dan dekripsi dari variabel lingkungan
+$encryptionKey = $envConfig['ENCRYPTION_KEY'];
+$decryptionKey = $envConfig['DECRYPTION_KEY'];
+
+if (!$encryptionKey || !$decryptionKey) {
+    die('Kunci enkripsi atau dekripsi tidak ditemukan. Pastikan variabel lingkungan ENCRYPTION_KEY dan DECRYPTION_KEY sudah diatur di file .env.');
+}
+
+function encryptData($data, $key)
+{
+    $ivSize = openssl_cipher_iv_length('aes-256-cbc');
+    $iv = openssl_random_pseudo_bytes($ivSize);
+    $encryptedData = openssl_encrypt($data, 'aes-256-cbc', $key, 0, $iv);
+    return base64_encode($iv . $encryptedData);
+}
+
+function decryptData($data, $key)
+{
+    $data = base64_decode($data);
+    $ivSize = openssl_cipher_iv_length('aes-256-cbc');
+    $iv = substr($data, 0, $ivSize);
+    $encryptedData = substr($data, $ivSize);
+
+    if ($iv === false || $encryptedData === false) {
+        // Gagal mendapatkan IV atau data yang dienkripsi
+        return false;
+    }
+
+    return openssl_decrypt($encryptedData, 'aes-256-cbc', $key, 0, $iv);
+}
+
+
+?>
