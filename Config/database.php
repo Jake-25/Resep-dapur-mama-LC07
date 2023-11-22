9919a2cd7de8a24a9007651fa6c8e4ce3e7be987
Jake-25 <96160692+Jake-25@users.noreply.github.com>
2023-11-22 16:34:31 +0700

    updated

--- Config/database.php
+++ Config/database.php
@@ -1,10 +1,10 @@
 <?php
 
 $config = [
-    'server' => getenv('DB_SERVER'),
+    'server' => getenv('127.0.0.1'),
     'username' => getenv('DB_USERNAME'),
     'password' => getenv('DB_PASSWORD'),
-    'database' => getenv('DB_DATABASE'),
+    'database' => getenv('database'),
 ];
 
 ?>
